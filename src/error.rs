9a91fa0d44275c121d3ba48b5eb4dde76219090c
Sergei <dev.sergeialimov@gmail.com>
2023-11-17 16:39:18 +0300

    basic error.rs

---
+++ src/error.rs
@@ -0,0 +1,60 @@
+use serde::Serialize;
+use std::convert::Infallible;
+use thiserror::Error;
+use warp::{http::StatusCode, Rejection, Reply};
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("wrong credentials")]
+    WrongCredentialsError,
+    #[error("jwt token not valid")]
+    JWTTokenCreationError,
+    #[error("no auth header")]
+    NoAuthHeaderError,
+    #[error("no permission")]
+    InvalidAuthHeaderError,
+    #[error("no permission")]
+    NoPermissionError
+}
+
+#[derive(Serialize, Debug)]
+struct ErrorResponse {
+   message: String,
+   status: String,
+}
+
+impl warp::reject::Reject for Error {}
+
+pub async fn handle_rejection(err:: Rejection) -> std::result::Result<impl Reply, Infallible> {
+    let (code, message) = if.err_is_not_found() {
+        (StatusCode::NOT_FOUND, "Not Found".to_string())
+    } else if let Some(e) = err.find::<Error>() {
+        match e {
+            Error::WrongCredentialsError => (StatusCode::FORBIDDEN, e.to_string()),
+            Error::NoPermissionError => (StatusCode::UNAUTHORIZED, e.to_string()),
+            Error::JWTTokenError => (StatusCode::UNAUTHORIZED, e.to_string()),
+            Error::JWTTokenCreationError => (StatusCode::UNAUTHORIZED, "Internal Server Error".to_string()),
+            _=> (StatusCode::BAD_REQUEST, e.to_string()),
+
+        }
+    } else if err.find::<warp::rejection::MethodNotAllowed>().is_some() {
+    (
+        StatusCode::METHOD_NOT_ALLOWED,
+        "Method Not Allowed".to_String(),
+    )
+    } else {
+        eprintln!("unhandled error: {:?}", err);
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Interval Server Error".to_string(),
+        )
+    };
+
+    let json = warp::reply::json(&ErrorResponse {
+        status: code.to_sting(),
+        message,
+    });
+
+    Ok(warp::reply::with_status(json, code))
+
+}
